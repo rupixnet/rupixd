f40b1c4da0d2e3258e1c29b0c9ca21e869dc1647
Rupix <rupix@rupixnet.com>
2026-03-13 14:34:51 -0600

    feat: sistema de niveles L1-L5 - constantes selladas en protocolo
    - LevelOro/Diamante/Platino/Rodio/Kings con ScriptPublicKey.Version
    - BlueScore de desbloqueo por halving (42M, 84M, 126M, 168M)
    - MaxSupply por nivel sellado en constants.go
    - BurnRatio=10 grabado en protocolo

--- domain/consensus/utils/constants/constants.go
+++ domain/consensus/utils/constants/constants.go
@@ -1,79 +1,43 @@
-package constants
+﻿package constants
 
 import "math"
 
 const (
-    // BlockVersion represents the current block version
     BlockVersion uint16 = 1
-
-    // MaxTransactionVersion is the current latest supported transaction version.
     MaxTransactionVersion uint16 = 0
-
-    // MaxScriptPublicKeyVersion is the current latest supported public key script version.
     MaxScriptPublicKeyVersion uint16 = 0
-
-    // RupiaPerRupix is the number of rupia in one rupix (1 RUPIX).
     RupiaPerRupix = 100_000_000
-
-    // MaxRupia is the maximum transaction amount allowed in rupia.
-	MaxRupia = uint64(42_000_000 * RupiaPerRupix)
-
-	// Burn antispam L1
-	MinBurnPerTx uint64 = 1_000  // rupias - piso minimo por transaccion
-	BurnPerByte  uint64 = 10     // rupias por byte - proporcional al tamano
-
-    // MaxTxInSequenceNum is the maximum sequence number the sequence field
-    // of a transaction input can be.
+    MaxRupia = uint64(42_000_000 * RupiaPerRupix)
     MaxTxInSequenceNum uint64 = math.MaxUint64
-
-    // SequenceLockTimeDisabled is a flag that if set on a transaction
-    // input's sequence number, the sequence number will not be interpreted
-    // as a relative locktime.
     SequenceLockTimeDisabled uint64 = 1 << 63
-
-    // SequenceLockTimeMask is a mask that extracts the relative locktime
-    // when masked against the transaction input sequence number.
     SequenceLockTimeMask uint64 = 0x00000000ffffffff
-
-    // LockTimeThreshold is the number below which a lock time is
-    // interpreted to be a DAA score.
     LockTimeThreshold = 5e11
-
-    // UnacceptedDAAScore is used to for UTXOEntries that were created by
-    // transactions in the mempool, or otherwise not-yet-accepted transactions.
     UnacceptedDAAScore = math.MaxUint64
-
-    // ========================================================
-    // RUPIX ANTI-SPAM PROTOCOL v1.0
-    // Protecciones grabadas en el protocolo desde el dia 1
-    // ========================================================
-
-    // MaxTxPayloadSize tamano maximo del Payload en bytes
-    MaxTxPayloadSize = 100
-
-    // MaxTxOutputs maximo de outputs por transaccion
-    MaxTxOutputs = 256
-
-    // MaxTxInputs maximo de inputs por transaccion
-    MaxTxInputs = 1000
-
-    // MinTxFeePerByte fee minimo en rupias por byte
-    MinTxFeePerByte = uint64(1)
-
     MinimumDifficultyTarget = "28269553036454149273332760011886696253239742350009903329945699220681916416"
-
-    // MaxBlockMassLimit masa maxima permitida por bloque
+    MinBurnPerTx uint64 = 1_000
+    BurnPerByte uint64 = 10
+    MaxTxPayloadSize = 100
+    MaxTxOutputs = 256
+    MaxTxInputs = 1000
+    MinTxFeePerByte = uint64(1)
     MaxBlockMassLimit = uint64(500_000)
-
-    // TxTypeNormal transferencia normal de RUPIX
     TxTypeNormal = uint8(0)
-
-    // TxTypeCoinbase recompensa del minero
     TxTypeCoinbase = uint8(1)
-
-    // TxTypeBurn quema de tokens para subir de nivel
     TxTypeBurn = uint8(2)
-
-    // TxTypeInvalid cualquier TX no reconocida
     TxTypeInvalid = uint8(255)
+    LevelOro uint16 = 0
+    LevelDiamante uint16 = 1
+    LevelPlatino uint16 = 2
+    LevelRodio uint16 = 3
+    LevelKings uint16 = 4
+    LevelDiamanteUnlockScore uint64 = 42_000_000
+    LevelPlatinoUnlockScore uint64 = 84_000_000
+    LevelRodioUnlockScore uint64 = 126_000_000
+    LevelKingsUnlockScore uint64 = 168_000_000
+    MaxSupplyOro uint64 = 42_000_000 * RupiaPerRupix
+    MaxSupplyDiamante uint64 = 2_100_000 * RupiaPerRupix
+    MaxSupplyPlatino uint64 = 210_000 * RupiaPerRupix
+    MaxSupplyRodio uint64 = 21_000 * RupiaPerRupix
+    MaxSupplyKings uint64 = 2_100 * RupiaPerRupix
+    BurnRatio uint64 = 10
 )
